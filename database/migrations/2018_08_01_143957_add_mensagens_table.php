827312acc3ada71f359d1b941cf812e424165b87
laura <lauumkk23@gmail.com>
2018-08-01 12:03:25 -0300

    final da aula 2

---
+++ database/migrations/2018_08_01_143957_add_mensagens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMensagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mensagem', function (Blueprint $table) {
+            $table ->increments('id');
+            $table ->string('titulo');
+            $table ->string('texto');
+            $table ->string('autor');
+            $table ->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mensagem', function (Blueprint $table) {
+            //
+        });
+    }
+}
